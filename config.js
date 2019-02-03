50253f24c031c080d3252d96e8a44a442604eab9
Gorka <gorka.pinol@gmail.com>
2019-02-03 10:40:23 +0100

    deployment on Heroku

---
+++ config.js
@@ -0,0 +1,8 @@
+//config.js
+
+module.exports = {
+    consumer_key: process.env.CKEY,
+    consumer_secret: process.env.CSECRET,
+    access_token: process.env.ATOKEN,
+    access_token_secret: process.env.ASECRET
+}
